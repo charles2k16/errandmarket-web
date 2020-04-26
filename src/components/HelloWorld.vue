f08b624515b60d357f5fd8c8daa4608d9f12f4b1
Charles Obeng <charlesobeng000@gmail.com>
2020-04-26 02:12:08 +0000

    initial setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,10 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      <el-button type="primary" icon="el-icon-star-off" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
